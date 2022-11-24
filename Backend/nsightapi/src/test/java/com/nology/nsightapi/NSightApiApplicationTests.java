0ae4a228103da515eaaa34b5727437f95d252a9d
William Busby <williambusby1999@gmail.com>
2022-11-24 14:02:06 +0000

    Fixed back end Java tests, still got to write fully

--- Backend/nsightapi/src/test/java/com/nology/nsightapi/NSightApiApplicationTests.java
+++ Backend/nsightapi/src/test/java/com/nology/nsightapi/NSightApiApplicationTests.java
@@ -2,9 +2,20 @@ package com.nology.nsightapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class NSightApiApplicationTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
