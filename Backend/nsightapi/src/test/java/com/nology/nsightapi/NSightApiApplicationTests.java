050b8bb3babf9b35e3a37d5af4509ccfb28786e2
William Busby <williambusby1999@gmail.com>
2022-11-24 14:14:53 +0000

    Changed testing to Mockito to test in github actions

--- Backend/nsightapi/src/test/java/com/nology/nsightapi/NSightApiApplicationTests.java
+++ Backend/nsightapi/src/test/java/com/nology/nsightapi/NSightApiApplicationTests.java
@@ -1,21 +1,19 @@
 package com.nology.nsightapi;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(NSightApiApplication.class)
 class NSightApiApplicationTests {
 
-	@LocalServerPort
-	private int port;
+	@Autowired
+	MockMvc mockMvc;
 
 	@Autowired
-	private TestRestTemplate restTemplate;
+	ObjectMapper mapper;
 
 	@Test
 	void contextLoads() {
