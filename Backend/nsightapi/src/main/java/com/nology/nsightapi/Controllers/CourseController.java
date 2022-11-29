c132f22e2ceee0b44ab730d0fac560ebb6a9438a
William Busby <williambusby1999@gmail.com>
2022-11-29 14:49:12 +0000

    Added methods and testing for course controller

--- Backend/nsightapi/src/main/java/com/nology/nsightapi/Controllers/CourseController.java
+++ Backend/nsightapi/src/main/java/com/nology/nsightapi/Controllers/CourseController.java
@@ -1,11 +1,17 @@
 package com.nology.nsightapi.Controllers;
 
+import com.mysql.cj.util.StringUtils;
 import com.nology.nsightapi.Entities.Course;
+import com.nology.nsightapi.Exceptions.BadRequestException;
+import com.nology.nsightapi.Exceptions.IdAlreadyExistsException;
+import com.nology.nsightapi.Exceptions.NotFoundException;
 import com.nology.nsightapi.Repositories.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -18,49 +24,80 @@ public class CourseController {
 
     @GetMapping("/courses")
     public ResponseEntity<List<Course>> getCourses() {
-        return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(repository.findAll());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
     }
 
     @GetMapping("/course/{id}")
     public ResponseEntity<Course> getCourseById(@PathVariable String id) {
-        return ResponseEntity.status(HttpStatus.OK).body(repository.findById(Integer.parseInt(id)));
-    }
-
-    @GetMapping("/course/instructor/{id}")
-    public ResponseEntity<List<Course>> getCoursesByInstructorId(@PathVariable String id) {
-        return ResponseEntity.status(HttpStatus.OK).body(repository.findCoursesByInstructorId(id));
+        try {
+            if (!StringUtils.isStrictlyNumeric(id)) {
+                throw new BadRequestException();
+            }
+            Course course = repository.findById(Integer.parseInt(id)).orElseThrow(NotFoundException::new);
+            return ResponseEntity.status(HttpStatus.OK).body(course);
+        } catch (NotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Course not found.");
+        } catch (BadRequestException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Non-numeric id entered.");
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        }
     }
 
     @PostMapping("/course")
     public ResponseEntity<String> createCourse(@RequestBody Course course) {
         try {
+            if (repository.findById(course.getId()).isPresent()) {
+                throw new IdAlreadyExistsException();
+            }
             repository.save(course);
             return ResponseEntity.status(HttpStatus.CREATED).body("Successfully created new course.");
+        } catch (IdAlreadyExistsException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_ACCEPTABLE, "Id already in use.");
+        } catch (DataIntegrityViolationException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing non-null values.");
         } catch (Exception e) {
-            System.out.println(e);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to create new course.");
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
 
     @PutMapping("/course")
-    public ResponseEntity<String> updateCourse(@RequestBody Course newCourse) {
+    public ResponseEntity<String> updateCourse(@RequestBody Course course) {
         try {
-            repository.save(newCourse);
-            return ResponseEntity.status(HttpStatus.CREATED).body("Updated course.");
+            if (repository.findById(course.getId()).isEmpty()) {
+                throw new NotFoundException();
+            }
+            repository.save(course);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Successfully updated course.");
+        } catch (NotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Course not found.");
+        } catch (DataIntegrityViolationException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Missing non-null values.");
         } catch (Exception e) {
-            System.out.println(e);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to update course.");
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
 
     @DeleteMapping("/course/{id}")
     public ResponseEntity<String> deleteCourse(@PathVariable String id) {
         try {
+            if (!StringUtils.isStrictlyNumeric(id)) {
+                throw new BadRequestException();
+            } else if (repository.findById(Integer.parseInt(id)).isEmpty()) {
+                throw new NotFoundException();
+            }
             repository.deleteById(Integer.parseInt(id));
-            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Deleted instructor.");
-        } catch (Exception e) {
-            System.out.println(e);
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to delete instructor.");
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Successfully deleted course.");
+        } catch (NotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Course not found.");
+        } catch (BadRequestException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Non-numeric id entered.");
+        }  catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
 }
